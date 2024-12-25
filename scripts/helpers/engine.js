0c315e88e83c6a38806163edb9f6013d8d45fa7c
wherewhere <qq2518766683@outlook.com>
2024-12-25 18:27:45 +0800

    添加归档页面

--- scripts/helpers/engine.js
+++ scripts/helpers/engine.js
@@ -1,5 +1,8 @@
 /* global hexo */
 "use strict";
 
-const htmlUrl = require("./html-url");
-hexo.extend.helper.register("html_url", htmlUrl);
+hexo.extend.helper.register("html_url", require("./html-url"));
+
+hexo.extend.helper.register('post_count', function (year) {
+  return this.site.posts.filter(post => this.date(post.date, 'YYYY') === year).count();
+});
