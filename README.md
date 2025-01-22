e939a7bf7d236154438c57f5651dc3729f20b748
wherewhere <qq2518766683@outlook.com>
2025-01-22 17:23:45 +0800

    添加 README 协议 启动图

---
+++ README.md
@@ -0,0 +1,11 @@
+# hexo-theme-skirky
+
+## 引用及参考
+- [Pjax](https://github.com/next-theme/pjax "pjax")
+- [Babel](https://github.com/babel/babel "babel")
+- [Pace.JS](https://github.com/CodeByZach/pace "pace")
+- [Vocabulary](https://github.com/creativecommons/vocabulary "vocabulary")
+- [NexT Theme](https://github.com/next-theme/hexo-theme-next "hexo-theme-next")
+- [Hexo Related Posts](https://github.com/sergeyzwezdin/hexo-related-posts "hexo-related-posts")
+- [Fluent UI System Icons](https://github.com/microsoft/fluentui-system-icons "fluentui-system-icons")
+- [Hexo Generator SearchDB](https://github.com/next-theme/hexo-generator-searchdb "hexo-generator-searchdb")
--- scripts/events/index.js
+++ scripts/events/index.js
@@ -9,3 +9,24 @@ hexo.extend.generator.register("webmanifest_generator", () => require("./lib/web
 hexo.extend.generator.register("pace_generator", () => renderGeneratorAsync(hexo, "third-party/pace.js", "pace-js", "pace.js"));
 hexo.extend.generator.register("pjax_generator", () => renderGeneratorAsync(hexo, "third-party/pjax.js", "@next-theme/pjax", "pjax.js"));
 hexo.extend.generator.register("search_generator", () => renderGeneratorAsync(hexo, "third-party/search.js", "hexo-generator-searchdb", "dist/search.js"));
+
+hexo.on("ready", () => {
+    if (!/^(g|s)/.test(hexo.env.cmd) || process.argv.includes('--disable-banner')) { return; }
+    const { version } = require('../../package.json');
+    hexo.log.info("==========================================");
+    hexo.log.info("                                          ");
+    hexo.log.info("  ████████████████      ████████████████  ");
+    hexo.log.info("  ████████████████      ████████████████  ");
+    hexo.log.info("  ██        ██████      ████████████████  ");
+    hexo.log.info("    ████    ██████                        ");
+    hexo.log.info("  ██  ██    ██  ██  ×  ██████████████████ ");
+    hexo.log.info("      ██    ██            ████    ████    ");
+    hexo.log.info("                                          ");
+    hexo.log.info("                                          ");
+    hexo.log.info(" └─              ─┘    └─              ─┘ ");
+    hexo.log.info("==========================================");
+    hexo.log.info("薇ㄦ猫猫送给小诗同志的礼物");
+    hexo.log.info("Hello from 2025");
+    hexo.log.info(`Version ${version}`);
+    hexo.log.info("==========================================");
+});
