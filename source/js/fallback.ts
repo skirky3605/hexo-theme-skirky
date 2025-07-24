5f2f2c9041f175fd9cc1dc94fa71c1347357e509
where where <qq2518766683@outlook.com>
2025-07-24 12:55:05 +0800

    移除 float 布局
    添加 HTML 5 兼容性

--- source/js/fallback.ts
+++ source/js/fallback.ts
@@ -10,26 +10,40 @@ interface IEHTMLStyleElement extends HTMLStyleElement {
   };
 }
 
+function addStyle(css: string) {
+  const style = document.createElement("style") as IEHTMLStyleElement;
+  const parentNode = document.head || document.scripts[document.scripts.length - 1].parentNode;
+  parentNode.appendChild(style);
+  if (style.styleSheet) {
+    style.type = "text/css";
+    style.styleSheet.cssText = css;
+  }
+  else {
+    style.innerText = css;
+  }
+}
+
 if (typeof (document as IEDocument).documentMode === "number") {
-  function addStyle(css: string) {
-    const style = document.createElement("style") as IEHTMLStyleElement;
-    const parentNode = document.head || document.scripts[document.scripts.length - 1].parentNode;
-    parentNode.appendChild(style);
-    if (style.styleSheet) {
-      style.type = "text/css";
-      style.styleSheet.cssText = css;
-    }
-    else {
-      style.innerText = css;
-    }
-  }
-  if ((document as IEDocument).documentMode < 10) {
-    addStyle(".main-grid .main-inner{width:calc(100% - 290px)}");
-  }
-  if ((document as IEDocument).documentMode < 7) {
+  const documentMode = (document as IEDocument).documentMode;
+  if (documentMode < 7) {
     addStyle("body .background{position:absolute}a.random-link{color:#000;background:none}.menu-item a .badge{right:auto}.sidebar-container{position:absolute}");
   }
-  if ((document as IEDocument).documentMode < 9) {
+  else if (documentMode == 9) {
+    addStyle("@media(min-width:768px){.main-grid .main-inner{float:left;width:calc(100% - 290px)}}");
+  }
+  else {
     addStyle(".sidebar-container{display:none}");
   }
+}
+else {
+  if (!window.CSS?.supports("display", "flex") && !("flex" in document.documentElement.style || "msFlex" in document.documentElement.style)) {
+    addStyle(".sidebar-container{display:none}");
+  }
+}
+
+if (!window.MediaList) {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/respond.js/dest/respond.min.js";
+  const parentNode = document.head || document.scripts[0].parentNode;
+  parentNode.appendChild(script);
 }
--- source/js/polyfill.ts
+++ source/js/polyfill.ts
@@ -62,4 +62,16 @@
       }
     };
   }
+  
+  if ((() => {
+    const element = document.createElement('a');
+    element.innerHTML = "<xyz></xyz>";
+    return element.childNodes.length !== 1;
+  })()) {
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/html5shiv";
+    script.async = false;
+    const parentNode = document.head || document.scripts[0].parentNode;
+    parentNode.appendChild(script);
+  }
 })();
